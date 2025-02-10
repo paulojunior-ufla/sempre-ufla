a744f4f8bb911fa5e727311423b440b49899bdb8
Jhennifer Silva <jhennifer.silva@estudante.ufla.br>
2025-02-09 21:41:47 -0300

    Responsividade e ajustes da Navbar completa.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,33 @@
 import logoEgressos from "../../assets/logo.png"
 import logoUfla from "../../assets/LogoUflaColorida.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./navbar.css"
 
-function Navbar (){
-    return(
+function Navbar() {
+    return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                
                 <a class="navbar-brand" href="#">
                     <img className="navbar-brand" src={logoUfla} alt="Logo da UFLA colorida" />
                     <img className="navbar-brand" src={logoEgressos} alt="Logo da Sempre Ufla" />
                 </a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav justify-content-end w-100">
-                        <a class="nav-item nav-link active" href="#">HOME <span class="sr-only"></span></a>
-                        <a class="nav-item nav-link" href="#">FERRAMENTAS E BENEFÍCIOS</a>
-                        <a class="nav-item nav-link" href="#">DÚVIDAS</a>
-                        <a class="nav-item nav-link" href="#">ENTRAR</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav w-100 justify-content-end">
+                        <a className="nav-item nav-link active" href="#">HOME</a>
+                        <a className="nav-item nav-link" href="#">FERRAMENTAS E BENEFÍCIOS</a>
+                        <a className="nav-item nav-link" href="#">DÚVIDAS</a>
+                        <a className="nav-item nav-link" href="#">ENTRAR</a>
                     </div>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
